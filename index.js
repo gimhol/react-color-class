13358e1c86cb0ccfd05def4e5a9eb8183c423f86
Gim <gimholg1992@foxmail.com>
2018-03-29 17:50:29 +0800

    babel

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import RGB from './RGB'
-import RGBA from './RGBA'
-import HSV from './HSV'
-export {
-  RGB,RGBA,HSV
-}
+module.exports = require('./lib/index');
+exports.default = require('./lib/index');
+exports.HSV = require('./lib/HSV');
+exports.RGB = require('./lib/RGB');
+exports.RGBA = require('./lib/RGBA');
---
+++ src/index.es6
@@ -0,0 +1,6 @@
+import RGB from './RGB'
+import RGBA from './RGBA'
+import HSV from './HSV'
+export {
+  RGB,RGBA,HSV
+}
